32d9da344a136a7801d3f1a490609880f1061e47
Ryan Hou <rhou7873@gmail.com>
2026-09-25 10:56:40 -0400

    Remove unused imports from merge migration

--- backend/migrations/versions/2026_09_25_1045-c1e7f8ff781d_merge_fork_and_upstream_migration_heads.py
+++ backend/migrations/versions/2026_09_25_1045-c1e7f8ff781d_merge_fork_and_upstream_migration_heads.py
@@ -6,9 +6,6 @@ Revises: 9c8b7a6d5e4f, a7c3e9f1b2d4
 """
 from typing import Sequence, Union
 
-from alembic import op
-import sqlalchemy as sa
-
 
 # revision identifiers, used by Alembic.
 revision: str = 'c1e7f8ff781d'
